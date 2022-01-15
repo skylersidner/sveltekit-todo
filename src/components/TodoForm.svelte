5b406abc0fca7d89e87ac341482d0b55415572aa
skylersidner <malevolentdragon@gmail.com>
2022-01-15 17:52:47 -0600

    adding todo store and using in TodoForm

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -1,15 +1,19 @@
 <script>
+    import {addTodo} from '../stores/todoStore.js';
+    
     let todo = '';
-
+    
     const handleSubmit = () => {
-        console.log('submitting');
+        addTodo(todo); // add to List
+        todo = ''; // rest List
+        console.log(todo);
     }
 </script>
 
 <form class="my-6" on:submit|preventDefault={handleSubmit}>
     <div class="flex flex-col text-sm mb-2">
         <label class="font-bold mb-2 text-gray-800" for="todo"></label>
-        <input class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg" type="text" name="todo" placeholder="What do you want to do?" />
+        <input class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg" type="text" bind:value={todo} name="todo" placeholder="What do you want to do?" />
     </div>
     <button type="submit" class="w-full shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4">Submit</button>
 </form>
